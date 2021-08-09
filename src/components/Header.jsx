0bdd46b8f276364d460712dcb2de8dbce080e76c
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-08-09 04:31:18 -0400

    Full Keeper app using Pre-Built React Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import FeaturedPlayListIcon from '@material-ui/icons/FeaturedPlayList';
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><FeaturedPlayListIcon />Keeper</h1>
     </header>
   );
 }
